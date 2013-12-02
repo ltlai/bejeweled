91da215960221fcda27f444734c4cd048649cb33
ltlai <leslie.t.lai@gmail.com>
2013-12-02 15:06:42 -0800

    Create empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bejeweled::Application.config.secret_key_base = '350cc04aa71f7e29b746dd39a6d217002ab3f8564b12894d4cfaaa6236afeabfe29d15f71af5d91e93466168b6f6944e194c4508c239504822f4ac69b570dc9d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Bejeweled::Application.config.session_store :cookie_store, key: '_bejeweled_session'
